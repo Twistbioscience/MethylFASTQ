2e6c3d886f1f1bd0e4d9ba31181dfa23970aaecf
Palak Sheth <psheth@twistbioscience.com>
2020-10-16 09:19:43 -0700

    WIP

--- bfx_ngs_te_simulate_methyl/sequencing.py
+++ bfx_ngs_te_simulate_methyl/sequencing.py
@@ -821,12 +821,20 @@ class TargetedFragmentSequencer(object):
         """ Returns a list containing all the information about cytosines """
         beta_score = lambda c: 0 if c.ncov == 0 else c.nmeth / c.ncov
 
-        data = [{'chrom': self.__chromoId,
-                 'pos': abs(position+1),
-                 'strand': cytosine.strand,
-                 'context': str(cytosine.context),
-                 'nmeth': cytosine.nmeth,
-                 'ncov': cytosine.ncov,
-                 'beta_score': beta_score(cytosine)} for position, cytosine in self.__cytosines.items()]
+        # data = [{'chrom': self.__chromoId,
+        #          'pos': abs(position+1),
+        #          'strand': cytosine.strand,
+        #          'context': str(cytosine.context),
+        #          'nmeth': cytosine.nmeth,
+        #          'ncov': cytosine.ncov,
+        #          'beta_score': beta_score(cytosine)} for position, cytosine in self.__cytosines.items()]
+
+        data = [(self.__chromoId,
+                 abs(position+1),
+                 cytosine.strand,
+                 str(cytosine.context),
+                 cytosine.nmeth,
+                 cytosine.ncov,
+                 beta_score(cytosine)) for position, cytosine in self.__cytosines.items()]
 
         return data
